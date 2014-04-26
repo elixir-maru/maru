50723b790a52ad96b01dc18c9e04d5f9586dea69
falood <falood@gmail.com>
2014-04-26 14:59:03 +0800

    update params test

--- test/helpers/lazymaru_params_test.exs
+++ test/helpers/lazymaru_params_test.exs
@@ -12,10 +12,10 @@ defmodule LazyHelper.ParamsTest do
   test "url params" do
     params = %{}
     conn = parse(conn(:get, "/?foo0=1&bar1=baz"))
-    requires :foo0, [type: Integer]
-    requires :foo1, [type: Integer, default: 2]
-    optional :bar0, [type: String, regexp: ~r/^[a-z]+$/]
-    optional :bar1, [type: String, regexp: ~r/^[a-z]+$/]
+    requires :foo0, type: Integer
+    requires :foo1, type: Integer, default: 2
+    optional :bar0, type: String, regexp: ~r/^[a-z]+$/
+    optional :bar1, type: String, regexp: ~r/^[a-z]+$/
     assert params == %{bar1: "baz", foo0: 1, foo1: 2}
   end
 
@@ -23,10 +23,10 @@ defmodule LazyHelper.ParamsTest do
     params = %{}
     headers = [{"content-type", "application/x-www-form-urlencoded"}]
     conn = parse(conn(:get, "/", "foo0=1&bar1=baz", headers: headers))
-    requires :foo0, [type: Integer]
-    requires :foo1, [type: Integer, default: 2]
-    optional :bar0, [type: String, regexp: ~r/^[a-z]+$/]
-    optional :bar1, [type: String, regexp: ~r/^[a-z]+$/]
+    requires :foo0, type: Integer
+    requires :foo1, type: Integer, default: 2
+    optional :bar0, type: String, regexp: ~r/^[a-z]+$/
+    optional :bar1, type: String, regexp: ~r/^[a-z]+$/
     assert params == %{bar1: "baz", foo0: 1, foo1: 2}
   end
 
