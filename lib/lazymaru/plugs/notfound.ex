7639f12c90316d6d726e1b3805213f8f719540c6
falood <falood@gmail.com>
2014-09-29 14:00:54 +0800

    bugfix: LazyException -> Lazymaru.exceptions

--- lib/lazymaru/plugs/notfound.ex
+++ lib/lazymaru/plugs/notfound.ex
@@ -1,6 +1,6 @@
 defmodule Lazymaru.Plugs.NotFound do
   def init(opts), do: opts
   def call(conn, _) do
-    LazyException.NotFound |> raise [method: conn.method, path_info: conn.path_info]
+    Lazymaru.Exceptions.NotFound |> raise [method: conn.method, path_info: conn.path_info]
   end
 end
