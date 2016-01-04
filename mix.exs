740f62e55850bb298a083810ba4bc8df5bfaf17d
falood <falood@gmail.com>
2016-01-04 15:43:34 +0800

    Release v0.9.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Maru.Mixfile do
   def project do
     [ app: :maru,
       name: "Maru",
-      version: "0.9.0",
+      version: "0.9.1",
       elixir: "~> 1.0",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
