5858194fa315228b3d803377e9fdbfb8c18dd0a0
falood <falood@gmail.com>
2016-03-05 10:46:47 +0800

    update docs dependencies

--- mix.exs
+++ mix.exs
@@ -27,12 +27,12 @@ defmodule Maru.Mixfile do
   end
 
   defp deps do
-    [ { :cowboy, "~> 1.0" },
-      { :plug,   "~> 1.0" },
-      { :poison, "~> 1.5 or ~> 2.0" },
-      { :inch_ex, only: :docs },
-      { :earmark, "~> 0.1", only: :docs },
-      { :ex_doc, "~> 0.10", only: :docs },
+    [ { :cowboy,  "~> 1.0" },
+      { :plug,    "~> 1.0" },
+      { :poison,  "~> 1.5 or ~> 2.0" },
+      { :inch_ex, "~> 0.5",  only: :docs },
+      { :earmark, "~> 0.2",  only: :docs },
+      { :ex_doc,  "~> 0.11", only: :docs },
     ]
   end
 
