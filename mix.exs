818074e741e8ae929f2dcd75dba7602654e9c43a
falood <falood@gmail.com>
2016-08-14 09:28:15 +0800

    drop elixir v1.0 and v1.1 support

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Maru.Mixfile do
     [ app: :maru,
       name: "Maru",
       version: "0.10.4-dev",
-      elixir: "~> 1.0",
+      elixir: "~> 1.2.3 or ~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps,
