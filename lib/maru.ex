93d855107231522eaa75dd3492fca3cf3d055838
falood <falood@gmail.com>
2017-03-18 09:24:12 -0700

    use standalone Maru.Supervisor

--- lib/maru.ex
+++ lib/maru.ex
@@ -1,5 +1,3 @@
-require Logger
-
 defmodule Maru do
   @moduledoc """
   This is documentation for maru.
@@ -8,7 +6,6 @@ defmodule Maru do
   """
 
   use Application
-  use Supervisor
 
   @doc """
   Maru version.
@@ -18,26 +15,12 @@ defmodule Maru do
     @version
   end
 
-  @default_ports http: 4000, https: 4040
-  @default_bind_addr {127, 0, 0, 1}
-
   @doc false
   def start(_type, _args) do
-    Application.ensure_all_started :plug
-
-    children =
-      for {module, options} <- servers() do
-        for protocol <- [:http, :https] do
-          if Keyword.has_key? options, protocol do
-            endpoint_spec(protocol, module, options[protocol])
-          end || []
-        end
-      end
-
-    opts = [strategy: :one_for_one, name: Maru.Supervisor]
-    Supervisor.start_link(List.flatten(children), opts)
+    Maru.Supervisor.start_link
   end
 
+  @doc false
   def servers do
     servers =
       Enum.filter(Application.get_all_env(:maru), fn {k, _} ->
@@ -54,26 +37,4 @@ defmodule Maru do
     end
   end
 
-  defp endpoint_spec(proto, module, opts) do
-    bind_addr = to_ip(opts[:bind_addr]) || opts[:ip] || @default_bind_addr
-
-    normalized_opts =
-      opts
-      |> Keyword.merge([port: to_port(opts[:port]) || @default_ports[proto]])
-      |> Keyword.merge([ip: bind_addr])
-      |> Keyword.delete(:bind_addr)
-    Logger.info "Starting #{module} with Cowboy on " <>
-                "#{proto}://#{:inet_parse.ntoa(bind_addr)}:#{opts[:port]}"
-    Plug.Adapters.Cowboy.child_spec(proto, module, [], normalized_opts)
-  end
-
-  defp to_port(nil),                        do: nil
-  defp to_port(port) when is_integer(port), do: port
-  defp to_port(port) when is_binary(port),  do: port |> String.to_integer
-
-  defp to_ip(nil), do: nil
-  defp to_ip(ip_addr) do
-    {:ok, inet_ip} = :inet_parse.ipv4_address(String.to_charlist(ip_addr))
-    inet_ip
-  end
 end
---
+++ lib/maru/supervisor.ex
@@ -0,0 +1,47 @@
+require Logger
+
+defmodule Maru.Supervisor do
+  use Supervisor
+
+  def start_link do
+    Supervisor.start_link(__MODULE__, [], name: __MODULE__)
+  end
+
+  def init([]) do
+    for {module, options} <- Maru.servers() do
+      for protocol <- [:http, :https] do
+        if Keyword.has_key? options, protocol do
+          endpoint_spec(protocol, module, options[protocol])
+        end || []
+      end
+    end
+    |> List.flatten
+    |> supervise(strategy: :one_for_one)
+  end
+
+  @default_ports http: 4000, https: 4040
+  @default_bind_addr {127, 0, 0, 1}
+
+  defp endpoint_spec(proto, module, opts) do
+    bind_addr = to_ip(opts[:bind_addr]) || opts[:ip] || @default_bind_addr
+
+    normalized_opts =
+      opts
+      |> Keyword.merge([port: to_port(opts[:port]) || @default_ports[proto]])
+      |> Keyword.merge([ip: bind_addr])
+      |> Keyword.delete(:bind_addr)
+    Logger.info "Starting #{module} with Cowboy on " <>
+                "#{proto}://#{:inet_parse.ntoa(bind_addr)}:#{opts[:port]}"
+    Plug.Adapters.Cowboy.child_spec(proto, module, [], normalized_opts)
+  end
+
+  defp to_port(nil),                        do: nil
+  defp to_port(port) when is_integer(port), do: port
+  defp to_port(port) when is_binary(port),  do: port |> String.to_integer
+
+  defp to_ip(nil), do: nil
+  defp to_ip(ip_addr) do
+    {:ok, inet_ip} = :inet_parse.ipv4_address(String.to_charlist(ip_addr))
+    inet_ip
+  end
+end
