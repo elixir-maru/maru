cb3c2b3b9e3db96ed48e30aab89a0d73d33242b6
falood <falood@gmail.com>
2017-07-17 19:19:14 -0700

    fix typo

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Maru.Mixfile do
     [ app: :maru,
       name: "Maru",
       version: "0.12.0-dev",
-      elixir: "~> 1.5.0-rc.1"
+      elixir: "~> 1.5.0-rc.1",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
       deps: deps(),
