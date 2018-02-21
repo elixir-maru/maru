98f07491868f5842d840b8bdced2a6d00cd11595
falood <falood@gmail.com>
2018-02-21 13:01:20 +0800

    update .formatter.exs

--- .formatter.exs
+++ .formatter.exs
@@ -7,7 +7,7 @@
   ],
   locals_without_parens: [
     mount: 1,
-    swagger: 1,
+    params: :*,
     helpers: 1,
     version: 1,
     plug: :*,
@@ -25,5 +25,28 @@
     desc: :*,
     detail: 1,
     status: 2
+  ],
+  export: [
+    locals_without_parens: [
+      mount: 1,
+      params: :*,
+      helpers: 1,
+      version: 1,
+      plug: :*,
+      plug_overridable: :*,
+      requires: :*,
+      optional: :*,
+      group: :*,
+      given: :*,
+      mutually_exclusive: 1,
+      exactly_one_of: 1,
+      at_least_one_of: 1,
+      all_or_none_of: 1,
+      prefix: :*,
+      rescue_from: :*,
+      desc: :*,
+      detail: 1,
+      status: 2
+    ]
   ]
 ]
